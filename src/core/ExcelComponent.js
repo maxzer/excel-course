b91669dfc12f7e4e2d3c855fcd25ee1552661999
max <maxzer7@gmail.com>
2020-08-28 00:27:09 +0300

    начал делать фреймворк для экселя

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,8 @@
+import {DomListener} from '@/core/DomListener';
+
+export class ExcelComponent extends DomListener {
+    // возврадащает шаблон компонента
+    toHTML() {
+        return '';
+    }
+}
